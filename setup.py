68a38d75ffce341d251102e8e16910549fd238f8
Sarathi Thirumalai Soundararajan <tssarathi5901@gmail.com>
2026-02-10 17:52:42 +1100

    chore(setup): bump version to 0.3 in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="ai-anime-recommender",
-    version="0.2",
+    version="0.3",
     author="Sarathi",
     packages=find_packages(),
     install_requires=requirements,
