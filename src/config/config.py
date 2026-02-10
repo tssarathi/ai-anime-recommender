6252265c1c96bb8cbdf38545f0bcb1ae07b2b2c9
Sarathi Thirumalai Soundararajan <tssarathi5901@gmail.com>
2026-02-10 16:54:58 +1100

    feat(config): add configuration file to load environment variables

---
+++ src/config/config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+MODEL_NAME = os.getenv("MODEL_NAME")
+HUGGINGFACE_API_TOKEN = os.getenv("HUGGINGFACEHUB_API_TOKEN")
