3b50285017df1ea55edf6d294b6d589e83f7e60e
Sarathi Thirumalai Soundararajan <tssarathi5901@gmail.com>
2026-02-11 16:27:05 +1100

    fix(etl): correct variable name and update CSV reading method in AnimeDataLoader

--- src/etl/data_loader.py
+++ src/etl/data_loader.py
@@ -1,15 +1,14 @@
-from _typeshed import Self
 import pandas as pd
 
 
 class AnimeDataLoader:
     def __init__(self, original_csv: str, processed_csv: str):
-        self.original_cdv = original_csv
-        Self.processed_csv = processed_csv
+        self.original_csv = original_csv
+        self.processed_csv = processed_csv
 
     def load_and_process(self):
         df = pd.read_csv(
-            self.original_csv, encoding="utf-8", error_bad_lines=False
+            self.original_csv, encoding="utf-8", on_bad_lines="skip"
         ).dropna()
         required_cols = {"Name", "Genres", "sypnopsis"}
 
@@ -29,4 +28,5 @@ class AnimeDataLoader:
             + df["sypnopsis"]
         )
 
-        df[["document"]].to_csv(self.processed_csv, index=False, encodings="utf-8")
+        df[["document"]].to_csv(self.processed_csv, index=False, encoding="utf-8")
+        return self.processed_csv
