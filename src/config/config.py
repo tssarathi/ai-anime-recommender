43fb7c2ba738dbf6ec43aa38330302808fee4fb1
Sarathi Thirumalai Soundararajan <tssarathi5901@gmail.com>
2026-02-11 23:33:32 +1100

    fix(config): set default model name in configuration to 'llama-3.1-8b-instant'

--- src/config/config.py
+++ src/config/config.py
@@ -5,5 +5,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-MODEL_NAME = os.getenv("MODEL_NAME")
+MODEL_NAME = os.getenv("MODEL_NAME", "llama-3.1-8b-instant")
 HUGGINGFACE_API_TOKEN = os.getenv("HUGGINGFACEHUB_API_TOKEN")
