f96ae32cfff9a7002e7360430daff6de9d22ac50
Sarathi Thirumalai Soundararajan <tssarathi5901@gmail.com>
2026-02-11 17:05:01 +1100

    refactor(vector_store): update import statement for Chroma from langchain_chroma

--- src/etl/vector_store.py
+++ src/etl/vector_store.py
@@ -1,5 +1,5 @@
+from langchain_chroma import Chroma
 from langchain_community.document_loaders.csv_loader import CSVLoader
-from langchain_community.vectorstores import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
 
